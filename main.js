a525c7d061efd78aa7950410cdc5a93a0e2bb800
Nuril-Nuzula <nurilnuzula222@gmail.com>
2022-11-23 10:37:43 +0700

    popup

--- main.js
+++ main.js
@@ -28,3 +28,8 @@ if (toastTrigger) {
     toast.show()
   })
 }
+
+// pop up
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
